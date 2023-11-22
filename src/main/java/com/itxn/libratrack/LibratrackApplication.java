10ba62049c67158c415d5f2a95f5ac935d1679fc
Igor Terekhin <igoryanterehin@gmail.com>
2023-11-22 20:58:05 +0000

    Initial commit

---
+++ src/main/java/com/itxn/libratrack/LibratrackApplication.java
@@ -0,0 +1,13 @@
+package com.itxn.libratrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibratrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibratrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itxn/libratrack/LibratrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itxn.libratrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibratrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
