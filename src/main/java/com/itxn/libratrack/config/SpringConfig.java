7d43d4ee91f20101573b32dba6186fbe5e53a56d
Igor Terekhin <igoryanterehin@gmail.com>
2023-12-05 00:32:12 +0400

    add app home page

--- src/main/java/com/itxn/libratrack/config/SpringConfig.java
+++ src/main/java/com/itxn/libratrack/config/SpringConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -45,5 +46,10 @@ public class SpringConfig implements WebMvcConfigurer {
     @Bean
     public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(dataSource());
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
     }
 }
