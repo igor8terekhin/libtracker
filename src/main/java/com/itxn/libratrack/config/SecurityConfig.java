9753069c06694e5d609fec2c1b765024f2065886
Igor Terekhin <igoryanterehin@gmail.com>
2024-03-19 22:10:21 +0000

    add correct annotation for security

--- src/main/java/com/itxn/libratrack/config/SecurityConfig.java
+++ src/main/java/com/itxn/libratrack/config/SecurityConfig.java
@@ -4,10 +4,12 @@ import com.itxn.libratrack.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PersonDetailsService personDetailsService;
